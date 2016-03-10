dd1db0379e878b09046157f165896ceb7315f185
MURILO MILLER PEREIRA SILVA <08215496695@est.udicentro.local>
2016-03-09 22:17:59 -0300

    Primeiro commit

---
+++ testedemesa1.php
@@ -0,0 +1,15 @@
+<?php
+$x = 5;
+$y = 0;
+while ($x > 2) {
+	print $x;
+	$y = $y + $x;
+	$x = $x - 1;
+}
+/*
+ X Recebe 5
+ Y Recebe 0
+ I Recebe 4
+ Tela
+ 	543
+*/
---
+++ testedemesa2.php
@@ -0,0 +1,15 @@
+<?php
+$x = 2;
+$y = 0;
+while ($x < 60) {
+	print "x\n";
+	$x = $x * 2;
+	$y = $y + 10;
+}
+/*
+	 X Recebe 2
+	 Y Recebe 10
+	 I Recebe 4
+	 Tela
+	 543 
+*/
